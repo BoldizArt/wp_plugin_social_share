ab6d3099234a181d4ec68ec5108bc1698a510aa2
BoldizArt <boldizar.santo@gmail.com>
2023-04-30 18:29:41 +0200

    Basic functional admin page.

--- src/SocialShare.php
+++ src/SocialShare.php
@@ -17,6 +17,9 @@ class SocialShare
     /** @param string $version; */
     public $version;
 
+    /** @param array $socialShareOptions; */
+    public $socialShareOptions;
+
     /**
      * Constructor
      */
@@ -25,11 +28,19 @@ class SocialShare
         // Set the plugin url
         $this->version = '0.0.1';
 
+        // Set the social share options
+        $this->socialShareOptions = get_option('social_share_options', []);
+
         // Add styles
-        add_action('init', [$this, 'addStyles']);
+        add_action('wp_enqueue_scripts', [$this, 'addStyles']);
+        add_action('admin_init', [$this, 'addAdminStyles']);
 
         // Add scripts
-        add_action('init', [$this, 'addScripts']);
+        add_action('wp_enqueue_scripts', [$this, 'addScripts']);
+        add_action('admin_enqueue_scripts', [$this, 'addAdminScripts']);
+
+        // Add admin menu item
+        add_action('admin_menu', [$this, 'socialShareAdminMenuItem']);
 
         // Test function
         add_action('init', [$this, 'testFunction']);
@@ -45,8 +56,177 @@ class SocialShare
         wp_enqueue_script('social_share_script');
     }
 
+
+    // create the main menu page
+    function socialShareAdminMenuItem() {
+        add_menu_page( 
+            __('Social share', 'social_share'),
+            __('Social share', 'social_share'),
+            'manage_options',
+            'social-share',
+            [$this, 'socialShareOptions'],
+            'dashicons-share',
+            25
+        );
+    }
+
+    // callback function for the main menu page
+    function socialShareOptions() {
+        // Set message
+        $messages = [];
+
+        // Check for post request
+        if (array_key_exists('social_share_options', $_POST)) {
+            $socialShare = $_POST['social_share_options'];
+            if (update_option('social_share_options', $socialShare)) {
+                $this->socialShareOptions = get_option('social_share_options', []);
+                $messages['success'] = __('Successfully saved.', 'social_share');
+            } else {
+                $messages['error'] = __('Something went wrong. Please try again.', 'social_share');
+            }
+        }
+        ?>
+        <div class="wrap">
+            <h1><?php _e('Social Share options', 'social_share'); ?></h1>
+            <?php if ($messages && is_array($messages) && !empty($messages)): ?>
+                <?php foreach ($messages as $type => $message): ?>
+                    <div class="notice notice-<?php echo $type; ?>"> 
+                        <p><strong><?php echo $message; ?></strong></p>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
+            <form method="post" class="social-share-form" action="<?php echo admin_url('/admin.php?page=social-share'); ?>">
+                <table class="form-table" role="presentation">
+                    <tbody>
+                        <tr>
+                            <th scope="row">
+                                <label><?php _e('Display on', 'social_share'); ?></label><br />
+                                <small>(<?php _e('Select one or more', 'social_share'); ?>)</small>
+                            </th>
+                            <td>
+                                <?php
+                                    $postTypes = get_post_types(['public' => true]);
+                                    foreach ($postTypes as $postType): 
+                                        if (in_array($postType, ['attachment', 'revision', 'nav_menu_item'])) {
+                                            continue;
+                                        }
+                                ?>
+                                    <label>
+                                        <input type="checkbox" 
+                                            name="social_share_options[post_types][]" 
+                                            value="<?php echo $postType; ?>" 
+                                            <?php echo isset($this->socialShareOptions['post_types']) && 
+                                                is_array($this->socialShareOptions['post_types']) && 
+                                                in_array($postType, $this->socialShareOptions['post_types']) ? 'checked' : ''; 
+                                            ?>
+                                        ><?php echo ucfirst($postType); ?></label><br>
+                                <?php endforeach; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><label for="buttonSize"><?php _e('Button size', 'social_share'); ?></label></th>
+                            <td>
+                                <?php $size = isset($this->socialShareOptions['button_size']) ? $this->socialShareOptions['button_size'] : 'small'; ?>
+                                <select name="social_share_options[button_size]" id="buttonSize">
+                                    <option value="small" <?php echo $size == 'small' ? 'selected' : ''; ?>>
+                                        <?php _e('Small', 'social_share'); ?>
+                                    </option>
+                                    <option value="medium" <?php echo $size == 'medium' ? 'selected' : ''; ?>>
+                                        <?php _e('Medium', 'social_share'); ?>
+                                    </option>
+                                    <option value="large" <?php echo $size == 'large' ? 'selected' : ''; ?>>
+                                        <?php _e('Large', 'social_share'); ?>
+                                    </option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">
+                                <label><?php _e('Social media share buttons', 'social_share'); ?></label>
+                            </th>
+                            <td>
+                                <?php
+                                    $socialMedias = [
+                                        'facebook' => __('Facebook', 'social_share'),
+                                        'twitter' => __('Twitter', 'social_share'),
+                                        'pinterest' => __('Pinterest', 'social_share'),
+                                        'linkedin' => __('LinkedIn', 'social_share'),
+                                        'whatsapp' => __('Whatsapp', 'social_share'),
+                                        'viber' => __('Viber', 'social_share')
+                                    ];
+                                    foreach ($socialMedias as $name => $socialMedia): 
+                                ?>
+                                    <div class="media <?php echo $name; ?>">
+                                        <p><strong> <?php echo $socialMedia; ?></strong></p>
+                                        <input type="checkbox" 
+                                            name="social_share_options[media][<?php echo $name; ?>][enabled]" 
+                                            value="1"
+                                            id="<?php echo $postType; ?>Enabled"
+                                            <?php echo isset($this->socialShareOptions['media'],
+                                                $this->socialShareOptions['media'][$name], 
+                                                $this->socialShareOptions['media'][$name]['enabled']) &&
+                                                $this->socialShareOptions['media'][$name]['enabled'] ? 'checked' : ''; 
+                                            ?>
+                                        >
+                                        <label for="<?php echo $postType; ?>Enabled">
+                                            <?php _e('Enabled', 'social_share'); ?>
+                                        </label>
+                                        <br>
+                                        <label for="<?php echo $name; ?>">Position</label><br>
+                                        <input name="social_share_options[media][<?php echo $name; ?>][position]" 
+                                            type="number" 
+                                            id="<?php echo $name; ?>" 
+                                            value="<?php echo isset($this->socialShareOptions['media'],
+                                                $this->socialShareOptions['media'][$name], 
+                                                $this->socialShareOptions['media'][$name]['position']) ?
+                                                $this->socialShareOptions['media'][$name]['position'] : 0; 
+                                            ?>"
+                                        >
+                                        <hr />
+                                    </div>
+                                <?php endforeach; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">
+                                <label><?php _e('Position of the social media share buttons', 'social_share'); ?></label><br />
+                                <small>(<?php _e('Select one or more', 'social_share'); ?>)</small>
+                            </th>
+                            <td>
+                                <?php $selectedPositions = isset($this->socialShareOptions['position']) && 
+                                    is_array($this->socialShareOptions['position']) ? 
+                                        $this->socialShareOptions['position'] : 
+                                        []; 
+                                ?>
+                                <?php $positions = [
+                                    'bellow' => __('Below the post title', 'social_share'),
+                                    'floating' => __('Floating on the left area', 'social_share'),
+                                    'after' => __('After the post content', 'social_share'),
+                                    'inside' => __('Inside the featured image', 'social_share'),
+                                ]; ?>
+                                <?php foreach ($positions as $key => $position): ?>
+                                <label>
+                                    <input type="checkbox" 
+                                        name="social_share_options[position][]" 
+                                        value="<?php echo $key; ?>" 
+                                        <?php echo in_array($key, $selectedPositions) ? 'checked' : ''; ?>
+                                    >
+                                    <?php echo $position; ?>
+                                </label>
+                                <br />
+                                <?php endforeach; ?>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p></form>
+            </form>
+        </div>  
+    <?php 
+    }
+
     /**
-     * Add admin stylesheets to the website
+     * Add stylesheets to the website
      */
     function addStyles()
     {
@@ -54,11 +234,27 @@ class SocialShare
     }
 
     /**
-     * Add admin scripts to the website
+     * Add admin stylesheets to the website
+     */
+    function addAdminStyles()
+    {
+        wp_register_style('social_share_style', plugins_url('./../assets/css/minified/social-share-admin.css', __FILE__), [], $this->version, 'all');
+    }
+
+    /**
+     * Add scripts to the website
      */
     function addScripts()
     {
         wp_register_script('social_share_script', plugins_url('./../assets/js/minified/social-share.js', __FILE__), [], $this->version, 'all');
+    }
+
+    /**
+     * Add admin scripts to the website
+     */
+    function addAdminScripts()
+    {
+        wp_register_script('social_share_script', plugins_url('./../assets/js/minified/social-share-admin.js', __FILE__), [], $this->version, 'all');
     }
 
     /** 
